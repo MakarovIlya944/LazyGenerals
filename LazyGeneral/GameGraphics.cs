01a2297d371abf174990513f17458b6a9efd3228
Илья <MakarovIlya944@yandex.ru>
2018-11-03 20:05:36 +0700

    Создал класс отвечающий за графику в игровом окне

---
+++ LazyGeneral/GameGraphics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyGeneral
+{
+	class GameGraphics
+	{
+		public readonly int widthNet, heightNet;
+
+		private bool PaintNet()
+		{
+
+			return false;
+		}
+
+	}
+}
