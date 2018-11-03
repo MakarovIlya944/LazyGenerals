809755307919485b9a6e07258a6509ec96d86311
Илья <MakarovIlya944@yandex.ru>
2018-11-03 19:50:58 +0700

    Добавил форму игрового окна, добавл фон на гланом окне, добавил кнопки на главном окне(не работающие)

--- LazyGeneral/MainWindow.cs
+++ LazyGeneral/MainWindow.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LazyGeneral
 {
-	public partial class Form1 : Form
+	public partial class MainWindow : Form
 	{
-		public Form1()
+		public MainWindow()
 		{
 			InitializeComponent();
 		}
@@ -33,6 +33,14 @@ namespace LazyGeneral
 		private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
 		{
 			
+		}
+
+		private void buttonStartGame_Click(object sender, EventArgs e)
+		{
+			//Application.OpenForms[0].Show(); открыть главную форму
+			GameWindow gw = new GameWindow();
+			gw.Show();
+			this.Hide();
 		}
 	}
 }
